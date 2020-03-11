2b5892227f08805ddc00689a765f0679ce458cc3
Zlatanlong <928339761@qq.com>
2020-03-11 11:37:42 +0800

    update .gitignore

--- mybatis-08/src/main/java/com/rui/pojo/Blog.java
+++ mybatis-08/src/main/java/com/rui/pojo/Blog.java
@@ -18,4 +18,45 @@ public class Blog {
 
     public Blog() {
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getCreatTime() {
+        return creatTime;
+    }
+
+    public void setCreatTime(Date creatTime) {
+        this.creatTime = creatTime;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+    
 }
